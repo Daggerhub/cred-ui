2b3c9f0bdb4c010deca2e3c2814f78dac122c3dd
Vishwash Bajpai <vishwash.bajpai87@gmail.com>
2022-05-26 22:42:40 +0530

    Adding Gilroy font and some basic setup

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeroSection from '../../components/HeroSection/index';
+
+const Homepage = () => {
+  return (
+    <HeroSection/>
+  )
+}
+
+export default Homepage
