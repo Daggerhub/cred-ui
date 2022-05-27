cd82e2d7f063769e105541b3520b392b5ae8885b
Vishwash Bajpai <vishwash.bajpai87@gmail.com>
2022-05-27 22:27:14 +0530

    adding the hero section with button and header as reusable components

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import Button from '../Button'
+import Header from '../Header'
+import "./heroSection.css"
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className='hero-section-wrapper'>
+      <Header/>
+      <div className="flex absolute-center hero-claim-label">
+        <div>pay credit card bill. earn guaranteed ₹200 back.</div>
+        <div className="claim-anchor">
+          claim now
+          <img
+            src="https://web-images.credcdn.in/_next/assets/images/home-page/arrow.png"
+            className="claim-arrow" alt='claimArrow'
+          />
+        </div>
+      </div>      
+      <div className='flex absolute-center flex-col hero-section max-width'>
+        <div className='hero-heading'>
+          rewards for paying credit card bills.
+        </div>
+        <div className='hero-subheading'>
+          join 9M+ members who win rewards and cashbacks everyday
+        </div>
+        <Button buttonText="Download Cred"/>
+      </div>
+    </div>
   )
 }
 
